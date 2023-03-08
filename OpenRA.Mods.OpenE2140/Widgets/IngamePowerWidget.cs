2071adf0a4b4bc9c6c39418ee79505014d6b92a6
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2023-03-08 19:10:29 +0100

    Sidebar: change game timer color based on player's faction
    + some cleanup

--- OpenRA.Mods.OpenE2140/Widgets/IngamePowerWidget.cs
+++ OpenRA.Mods.OpenE2140/Widgets/IngamePowerWidget.cs
@@ -29,7 +29,7 @@ public class IngamePowerWidget : WorldLabelWithTooltipWidget, IFactionSpecificWi
 	{
 	}
 
-	string[] IFactionSpecificWidget.FieldsToOverride => new[] { nameof(this.NormalPowerColor), nameof(this.CriticalPowerColor) };
+	string[] IFactionSpecificWidget.FieldsToOverride => new[] { nameof(this.NormalPowerColor), nameof(this.CriticalPowerColor), nameof(TextColor) };
 
 	string IFactionSpecificWidget.Identifier => this.Identifier;
 }
