282f04857f7f78d09b815ef49051ff946070247e
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2023-04-10 23:32:38 +0200

    Render cloaked units as shadow of their body when rendering to owner

---
+++ OpenRA.Mods.OpenE2140/Traits/Rendering/WithCloakShadow.cs
@@ -0,0 +1,72 @@
+﻿#region Copyright & License Information
+
+/*
+ * Copyright (c) The OpenE2140 Developers and Contributors
+ * This file is part of OpenE2140, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+
+#endregion
+
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Traits.Render;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Rendering;
+
+public class WithCloakShadowInfo : TraitInfo, Requires<CloakInfo>
+{
+	[Desc("Color to draw shadow.")]
+	public readonly Color ShadowColor = Color.FromArgb(140, 0, 0, 0);
+
+	public override object Create(ActorInitializer init)
+	{
+		return new WithCloakShadow(init.Self, this);
+	}
+}
+
+public class WithCloakShadow : IRenderModifier
+{
+	private readonly WithCloakShadowInfo info;
+	private readonly Cloak cloak;
+	private readonly float3 shadowColor;
+	private readonly float shadowAlpha;
+
+	public WithCloakShadow(Actor self, WithCloakShadowInfo info)
+	{
+		this.info = info;
+		this.cloak = self.Trait<Cloak>();
+		this.shadowColor = new float3(info.ShadowColor.R, info.ShadowColor.G, info.ShadowColor.B) / 255f;
+		this.shadowAlpha = info.ShadowColor.A / 255f;
+	}
+
+	IEnumerable<IRenderable> IRenderModifier.ModifyRender(Actor self, WorldRenderer wr, IEnumerable<IRenderable> renderables)
+	{
+		if (self.World.FogObscures(self) || self.World.ShroudObscures(self.CenterPosition))
+			return renderables;
+
+		if (!this.cloak.Cloaked || !this.cloak.IsVisible(self, self.World.RenderPlayer))
+		{
+			return renderables;
+		}
+
+		return self.TraitsImplementing<RenderSprites>()
+			.SelectMany(rs => rs.Render(self, wr))
+			.OfType<IModifyableRenderable>()
+			.Select(
+				modifyableRenderable => modifyableRenderable.WithTint(this.shadowColor, modifyableRenderable.TintModifiers | TintModifiers.ReplaceColor)
+					.WithAlpha(this.shadowAlpha)
+					.AsDecoration()
+			);
+	}
+
+	IEnumerable<Rectangle> IRenderModifier.ModifyScreenBounds(Actor self, WorldRenderer wr, IEnumerable<Rectangle> bounds)
+	{
+		return bounds;
+	}
+}
