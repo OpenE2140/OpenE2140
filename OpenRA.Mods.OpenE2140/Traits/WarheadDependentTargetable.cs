4e5a9eb750cca03b536489a33b6ecb96b725f8b5
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2023-06-03 00:26:55 +0200

    New Targetable trait: makes unit targetable depending on viewer's warheads

---
+++ OpenRA.Mods.OpenE2140/Traits/WarheadDependentTargetable.cs
@@ -0,0 +1,37 @@
+﻿using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits;
+
+[Desc("Makes actor targetability dependent on viewer's warheads")]
+public class WarheadDependentTargetableInfo : TargetableInfo
+{
+	[Desc("List of viewer warheads that makes it not able to target this actor.")]
+	public readonly string[] InvalidViewerWarheads = Array.Empty<string>();
+
+	public override object Create(ActorInitializer init) { return new WarheadDependentTargetable(this); }
+}
+
+public class WarheadDependentTargetable : Targetable
+{
+	private readonly WarheadDependentTargetableInfo info;
+
+	public WarheadDependentTargetable(WarheadDependentTargetableInfo info)
+		: base(info)
+	{
+		this.info = info;
+	}
+
+	public override bool TargetableBy(Actor self, Actor viewer)
+	{
+		if (this.IsTraitDisabled)
+			return false;
+
+		// Actor is targetable by the viewer only if any armament has weapon with invalid warhead
+		var invalidArmaments = viewer.TraitsImplementing<Armament>()
+			.Where(a => a.Weapon.Warheads.Any(a => this.info.InvalidViewerWarheads.Any(w => a.GetType().Name.StartsWith(w))) == true);
+		if (invalidArmaments.Any())
+			return false;
+
+		return base.TargetableBy(self, viewer);
+	}
+}
