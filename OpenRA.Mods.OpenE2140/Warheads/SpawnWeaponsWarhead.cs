dc9ce55dc68e9cfcf734e2efd5d48119726ea89e
Dzierzan <dzierzan.12111994@gmail.com>
2023-09-13 21:56:56 +0200

    Added support for Inaccuracies for SpawnWeaponsWarhead

--- OpenRA.Mods.OpenE2140/Warheads/SpawnWeaponsWarhead.cs
+++ OpenRA.Mods.OpenE2140/Warheads/SpawnWeaponsWarhead.cs
@@ -13,6 +13,8 @@
 
 using JetBrains.Annotations;
 using OpenRA.GameRules;
+using OpenRA.Effects;
+using OpenRA.Mods.Common;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Mods.OpenE2140.Effects;
 using OpenRA.Mods.OpenE2140.Extensions;
@@ -27,14 +29,17 @@ public class SpawnWeaponsWarhead : EffectWarhead, IRulesetLoaded<WeaponInfo>
 	[Desc("The weapons to spawn.")]
 	public readonly string[] Weapons = Array.Empty<string>();
 
-	[Desc("Delay in ticks before applying the warhead effect.", "0 = instant (old model).")]
+	[Desc("Delay in ticks before applying the warhead effect.", "0 = instant.")]
 	public readonly int[] Delays = { 0 };
 
-	[Desc("The amount of projectile pieces to produce. Two values indicate a range.")]
-	public readonly int[] Pieces = { 3, 10 };
+	[Desc("The amount of projectile pieces to produce.")]
+	public readonly int[] Pieces = { 1 };
 
 	[Desc("The minimum and maximum distances the projectile may travel.")]
 	public readonly WDist[] Range = { WDist.FromCells(2), WDist.FromCells(5) };
+
+	[Desc("The maximum inaccuracy of the effect spawn position relative to actual impact position for each weapon.")]
+	public readonly WDist[] Inaccuracies = { WDist.Zero };
 
 	[Desc(
 		"Whether to use Damage, Inaccuracy and Range modifiers from source actor. If the source actor does not exist in the world, "
@@ -49,37 +54,47 @@ public class SpawnWeaponsWarhead : EffectWarhead, IRulesetLoaded<WeaponInfo>
 		if (this.Weapons.Length != this.Delays.Length)
 			throw new YamlException($"Length of: '{nameof(this.Weapons)}' and '{nameof(this.Delays)}' must be equal.");
 
+		if (this.Inaccuracies.Length > 1 && this.Inaccuracies.Length < this.Weapons.Length)
+			throw new YamlException($"Length of: '{nameof(this.Inaccuracies)}' must be equal to '{nameof(this.Weapons)}' or default single value.");
+
 		this.WeaponInfos = this.Weapons.Select(
-				w =>
-				{
-					var weaponToLower = w.ToLowerInvariant();
+			w =>
+			{
+				var weaponToLower = w.ToLowerInvariant();
 
-					if (!rules.Weapons.TryGetValue(weaponToLower, out var weapon))
-						throw new YamlException($"Weapons Ruleset does not contain an entry '{weaponToLower}'");
+				if (!rules.Weapons.TryGetValue(weaponToLower, out var weapon))
+					throw new YamlException($"Weapons Ruleset does not contain an entry '{weaponToLower}'");
 
-					return weapon;
-				}
-			)
+				return weapon;
+			})
 			.ToArray();
 	}
 
 	public override void DoImpact(in Target target, WarheadArgs args)
 	{
 		var firedBy = args.SourceActor;
-		var pos = new WPos(target.CenterPosition.X, target.CenterPosition.Y, Math.Max(0, target.CenterPosition.Z));
+		// var pos = new WPos(target.CenterPosition.X, target.CenterPosition.Y, Math.Max(0, target.CenterPosition.Z));
 		var world = firedBy.World;
+		var inaccuracies = this.Inaccuracies.Length > 1 
+			? this.Inaccuracies 
+			: Enumerable.Repeat(this.Inaccuracies[0], this.WeaponInfos.Length).ToArray();
 
 		if (!this.IsValidAgainst(target, firedBy))
 			return;
 
 		for (var i = 0; this.WeaponInfos.Length > i; i++)
 		{
-			var pieces = world.SharedRandom.Next(this.Pieces[0], this.Pieces[1]);
+			var weaponImpactPosition = new WPos(target.CenterPosition.X, target.CenterPosition.Y, Math.Max(0, target.CenterPosition.Z));
+			var pieces = Util.RandomInRange(world.SharedRandom, this.Pieces);
 			var range = world.SharedRandom.Next(this.Range[0].Length, this.Range[1].Length);
+			var inaccuracy = inaccuracies[i];
+
+			weaponImpactPosition += WVec.FromPDF(world.SharedRandom, 2) * inaccuracy.Length / 1024;
 
 			for (var j = 0; pieces > j; j++)
 			{
 				var rotation = WRot.FromYaw(new WAngle(world.SharedRandom.Next(1024)));
+				var position = weaponImpactPosition;
 
 				var projectileArgs = new
 				{
@@ -89,14 +104,14 @@ public class SpawnWeaponsWarhead : EffectWarhead, IRulesetLoaded<WeaponInfo>
 						Weapon = this.WeaponInfos[i],
 						Facing = new WAngle(world.SharedRandom.Next(1024)),
 						CurrentMuzzleFacing = () => WAngle.Zero,
-						Source = pos,
-						CurrentSource = () => pos,
+						Source = position,
+						CurrentSource = () => position,
 						SourceActor = args.SourceActor,
-						PassiveTarget = pos + new WVec(range, 0, 0).Rotate(rotation)
+						PassiveTarget = position + new WVec(range, 0, 0).Rotate(rotation)
 					}
 				};
 
-				if (this.UseAttackerModifiers)
+				if (this.UseAttackerModifiers && firedBy.IsInWorld)
 				{
 					projectileArgs.Args.DamageModifiers =
 						firedBy.TryGetTraitsImplementing<IFirepowerModifier>().Select(a => a.GetFirepowerModifier()).ToArray();
@@ -104,25 +119,35 @@ public class SpawnWeaponsWarhead : EffectWarhead, IRulesetLoaded<WeaponInfo>
 					projectileArgs.Args.InaccuracyModifiers =
 						firedBy.TryGetTraitsImplementing<IInaccuracyModifier>().Select(a => a.GetInaccuracyModifier()).ToArray();
 
-					projectileArgs.Args.RangeModifiers = firedBy.TryGetTraitsImplementing<IRangeModifier>().Select(a => a.GetRangeModifier()).ToArray();
+					projectileArgs.Args.RangeModifiers = 
+						firedBy.TryGetTraitsImplementing<IRangeModifier>().Select(a => a.GetRangeModifier()).ToArray();
 				}
 
-				var delayedTarget = target;
+				var positionedTarget = Target.FromPos(weaponImpactPosition);
 
 				world.AddFrameEndTask(
-					x =>
+					_ =>
 					{
-						if (projectileArgs.Args.Weapon.Projectile != null)
+						if (projectileArgs.Args.Weapon.Projectile is null)
+						{
+							var warheadArgs = new WarheadArgs(projectileArgs.Args);
+
+							foreach (var warhead in projectileArgs.Args.Weapon.Warheads)
+							{
+								if (projectileArgs.Delay > 0)
+									world.AddFrameEndTask(w => w.Add(new DelayedImpact(projectileArgs.Delay, warhead, positionedTarget, warheadArgs)));
+								else
+									warhead.DoImpact(positionedTarget, warheadArgs);
+							}
+						}
+						else
 						{
 							var projectile = projectileArgs.Args.Weapon.Projectile.Create(projectileArgs.Args);
 
-							if (projectile != null)
-							{
-								if (projectileArgs.Delay > 0)
-									world.AddFrameEndTask(w => w.Add(new DelayedProjectile(projectile, projectileArgs.Delay)));
-								else
-									world.Add(projectile);
-							}
+							if (projectileArgs.Delay > 0)
+								world.AddFrameEndTask(w => w.Add(new DelayedProjectile(projectile, projectileArgs.Delay)));
+							else
+								world.Add(projectile);
 						}
 					}
 				);
