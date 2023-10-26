87dbafa37fb1069c5c2f8857b41f6a6bbe3ae8ae
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2023-10-26 22:26:40 +0200

    Trait for previewing rectangle based on building footprint

---
+++ OpenRA.Mods.OpenE2140/Traits/Mcu/BuildingPreviewRectangleRenderable.cs
@@ -0,0 +1,65 @@
+﻿#region Copyright & License Information
+
+/*
+ * Copyright (c) The OpenE2140 Developers and Contributors
+ * This file is part of OpenE2140, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+
+#endregion
+
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Graphics;
+using OpenRA.Primitives;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Mcu;
+
+public class BuildingPreviewRectangleRenderable : IRenderable, IFinalizedRenderable
+{
+	private readonly Rectangle decorationBounds;
+	private readonly Color color;
+
+	public BuildingPreviewRectangleRenderable(Actor actor, Rectangle decorationBounds, Color color)
+		: this(actor.CenterPosition, decorationBounds, color) { }
+
+	public BuildingPreviewRectangleRenderable(WPos pos, Rectangle decorationBounds, Color color)
+	{
+		this.Pos = pos;
+		this.decorationBounds = decorationBounds;
+		this.color = color;
+	}
+
+	public WPos Pos { get; }
+
+	public int ZOffset => 0;
+	public bool IsDecoration => true;
+
+	public IRenderable WithZOffset(int newOffset) { return this; }
+	public IRenderable OffsetBy(in WVec vec) { return new SelectionBoxAnnotationRenderable(this.Pos + vec, this.decorationBounds, this.color); }
+	public IRenderable AsDecoration() { return this; }
+
+	public IFinalizedRenderable PrepareRender(WorldRenderer wr) { return this; }
+	public void Render(WorldRenderer wr)
+	{
+		var tl = WorldToViewPx(new float2(this.decorationBounds.Left, this.decorationBounds.Top)).ToFloat2();
+		var br = WorldToViewPx(new float2(this.decorationBounds.Right, this.decorationBounds.Bottom)).ToFloat2();
+		var tr = new float2(br.X, tl.Y);
+		var bl = new float2(tl.X, br.Y);
+		var u = new float2(24, 0);
+		var v = new float2(0, 24);
+
+		var cr = Game.Renderer.RgbaColorRenderer;
+		cr.DrawLine(new float3[] { tl + u, tl, tl + v }, 1, this.color, true);
+		cr.DrawLine(new float3[] { tr - u, tr, tr + v }, 1, this.color, true);
+		cr.DrawLine(new float3[] { br - u, br, br - v }, 1, this.color, true);
+		cr.DrawLine(new float3[] { bl + u, bl, bl - v }, 1, this.color, true);
+
+		int2 WorldToViewPx(in float3 world) { return (world - wr.Viewport.TopLeft).XY.ToInt2(); }
+	}
+
+	public void RenderDebugGeometry(WorldRenderer wr) { }
+	public Rectangle ScreenBounds(WorldRenderer wr) { return Rectangle.Empty; }
+}
---
+++ OpenRA.Mods.OpenE2140/Traits/Mcu/RectanglePlaceBuildingPreview.cs
@@ -0,0 +1,88 @@
+﻿#region Copyright & License Information
+
+/*
+ * Copyright (c) The OpenE2140 Developers and Contributors
+ * This file is part of OpenE2140, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+
+#endregion
+
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Orders;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Mcu;
+
+public class RectanglePlaceBuildingPreviewInfo : TraitInfo<RectanglePlaceBuildingPreview>, IPlaceBuildingPreviewGeneratorInfo
+{
+	IPlaceBuildingPreview IPlaceBuildingPreviewGeneratorInfo.CreatePreview(WorldRenderer wr, ActorInfo ai, TypeDictionary init)
+	{
+		return new RectanglePlaceBuildingPreviewPreview(wr, ai);
+	}
+}
+
+public class RectanglePlaceBuildingPreview { }
+
+public class RectanglePlaceBuildingPreviewPreview : IPlaceBuildingPreview
+{
+	private readonly WVec centerOffset;
+	private readonly int2 topLeftScreenOffset;
+
+	public RectanglePlaceBuildingPreviewPreview(WorldRenderer wr, ActorInfo ai)
+	{
+		var world = wr.World;
+		this.centerOffset = ai.TraitInfo<BuildingInfo>().CenterOffset(world);
+		this.topLeftScreenOffset = -wr.ScreenPxOffset(this.centerOffset);
+	}
+
+	IEnumerable<IRenderable> IPlaceBuildingPreview.Render(WorldRenderer wr, CPos topLeft, Dictionary<CPos, PlaceBuildingCellType> footprint)
+	{
+		var b = GetBounds(footprint);
+		var rect = Rectangle.FromLTRB(b.topLeft.X, b.topLeft.Y, b.bottomRight.X, b.bottomRight.Y);
+
+		// Convert from WDist to pixels
+		var size = new int2(rect.Width * wr.TileSize.Width / wr.TileScale, rect.Height * wr.TileSize.Height / wr.TileScale);
+
+		var xy = wr.ScreenPxPosition(b.topLeft);
+		var renderRect = new Rectangle(xy, new Size(size.X, size.Y));
+
+		var color = footprint.Values.Any(c => c.HasFlag(PlaceBuildingCellType.Invalid)) ? Color.Red : Color.Green;
+
+		yield return new BuildingPreviewRectangleRenderable(b.topLeft, renderRect, color);
+	}
+
+	private static (WPos topLeft, WPos bottomRight) GetBounds(Dictionary<CPos, PlaceBuildingCellType> footprint)
+	{
+		var left = int.MaxValue;
+		var right = int.MinValue;
+		var top = int.MaxValue;
+		var bottom = int.MinValue;
+
+		foreach (var p in footprint.Keys)
+		{
+			left = Math.Min(left, p.X);
+			right = Math.Max(right, p.X);
+			top = Math.Min(top, p.Y);
+			bottom = Math.Max(bottom, p.Y);
+		}
+
+		return (new WPos(1024 * left, 1024 * top, 0),
+			new WPos(1024 * right + 1024, 1024 * bottom + 1024, 0));
+	}
+
+	IEnumerable<IRenderable> IPlaceBuildingPreview.RenderAnnotations(WorldRenderer wr, CPos topLeft)
+	{
+		return Enumerable.Empty<IRenderable>();
+	}
+
+	void IPlaceBuildingPreview.Tick() { }
+
+	int2 IPlaceBuildingPreview.TopLeftScreenOffset => this.topLeftScreenOffset;
+}
+
