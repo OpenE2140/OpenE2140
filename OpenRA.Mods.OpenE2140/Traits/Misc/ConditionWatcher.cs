da82031ae36638d8f355bddc292fa71065112d4b
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2023-11-12 20:31:30 +0100

    ConditionWatcher: helper class for watching condition changes

---
+++ OpenRA.Mods.OpenE2140/Traits/Misc/ConditionWatcher.cs
@@ -0,0 +1,55 @@
+﻿using OpenRA.Support;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Misc;
+
+public class ConditionWatcher : IObservesVariables
+{
+	private readonly Dictionary<BooleanExpression, Handler> handlers = new Dictionary<BooleanExpression, Handler>();
+
+	public ConditionWatcher Watch(BooleanExpression condition, Action<bool> onChange)
+	{
+		if (this.handlers.ContainsKey(condition))
+			throw new InvalidOperationException($"Handler already defined for condition: {condition}");
+
+		this.handlers.Add(condition, new Handler(condition, onChange));
+		return this;
+	}
+
+	public IEnumerable<VariableObserver> GetVariableObservers()
+	{
+		foreach (var handler in this.handlers.Values)
+		{
+			yield return handler.CreateObserver();
+		}
+	}
+
+	public bool IsEnabled(BooleanExpression condition)
+	{
+		if (!this.handlers.TryGetValue(condition, out var handler))
+			throw new InvalidOperationException($"Could not find handler for condition: {condition}");
+
+		return handler.IsEnabled;
+	}
+
+	private record Handler(BooleanExpression Condition, Action<bool> OnChange)
+	{
+		public bool IsEnabled { get; private set; }
+
+		public VariableObserver CreateObserver()
+		{
+			return new VariableObserver(this.Observer, this.Condition.Variables);
+		}
+
+		private void Observer(Actor self, IReadOnlyDictionary<string, int> conditions)
+		{
+			var newState = this.Condition.Evaluate(conditions);
+			if (newState != this.IsEnabled)
+			{
+				this.OnChange(newState);
+
+				this.IsEnabled = newState;
+			}
+		}
+	}
+}
