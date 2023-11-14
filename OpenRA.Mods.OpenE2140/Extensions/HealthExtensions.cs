1bcec8d11971c7823423f7ce9a80909e98bfe015
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2023-11-14 20:59:11 +0100

    GetHPPercentage extension method for IHealth

---
+++ OpenRA.Mods.OpenE2140/Extensions/HealthExtensions.cs
@@ -0,0 +1,11 @@
+﻿using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Extensions;
+
+public static class HealthExtensions
+{
+	public static int GetHPPercentage(this IHealth health)
+	{
+		return health.HP * 100 / health.MaxHP;
+	}
+}
