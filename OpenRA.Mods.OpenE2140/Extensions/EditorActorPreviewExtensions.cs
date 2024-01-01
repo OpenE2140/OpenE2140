953ac53e3b504c4454a84c0aff5ed2b6e278ed2f
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-01-01 18:50:11 +0100

    Extensions for EditorActorPreview simplifying access to Init objects

---
+++ OpenRA.Mods.OpenE2140/Extensions/EditorActorPreviewExtensions.cs
@@ -0,0 +1,17 @@
+﻿using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Extensions;
+
+public static class EditorActorPreviewExtensions
+{
+	public static bool TryGetInit<T>(this EditorActorPreview editorActorPreview, out T init) where T : ActorInit, ISingleInstanceInit
+	{
+		init = editorActorPreview.GetInitOrDefault<T>();
+		return init != null;
+	}
+
+	public static bool HasInit<T>(this EditorActorPreview editorActorPreview) where T : ActorInit, ISingleInstanceInit
+	{
+		return editorActorPreview.GetInitOrDefault<T>() != null;
+	}
+}
