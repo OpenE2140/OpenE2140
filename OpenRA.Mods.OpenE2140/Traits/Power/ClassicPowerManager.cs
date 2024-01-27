d673f92e26d66cc30d5442e7d8e868b317fc1eb9
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-01-27 18:56:42 +0100

    ClassicPowerManager with the classic low power behavior

---
+++ OpenRA.Mods.OpenE2140/Traits/Power/ClassicPowerManager.cs
@@ -0,0 +1,92 @@
+﻿using JetBrains.Annotations;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Power;
+
+[TraitLocation(SystemActors.Player)]
+[Desc("Earth specific variant of the PowerManager trait. This is the classic version, with the same logic as in E2140.")]
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public class ClassicPowerManagerInfo : PowerManagerBaseInfo, IRulesetLoaded
+{
+	[Desc("Interval (in ticks) at which to to cycle the power actors on low power.")]
+	public readonly int PowerCycleInterval = 9;
+
+	[Desc($"Interval (in ticks) during which actors in low power state are getting power. Has to be lower than {nameof(PowerCycleInterval)}, but higher than 0")]
+	public readonly int PowerOnInterval = 1;
+
+	public override object Create(ActorInitializer init)
+	{
+		return new ClassicPowerManager(init.Self, this);
+	}
+
+	void IRulesetLoaded<ActorInfo>.RulesetLoaded(Ruleset rules, ActorInfo info)
+	{
+		if (this.PowerOnInterval >= this.PowerCycleInterval || this.PowerOnInterval < 0)
+			throw new YamlException($"{nameof(this.PowerOnInterval)} has to be higher than 0 and lower than {nameof(this.PowerCycleInterval)}.");
+	}
+}
+
+public class ClassicPowerManager : PowerManagerBase
+{
+	private readonly ClassicPowerManagerInfo info;
+	private bool powerOn = true;
+
+	public ClassicPowerManager(Actor self, ClassicPowerManagerInfo info)
+		: base(self, info)
+	{
+		this.info = info;
+	}
+
+	protected override PlayerPowerState TickInner(Actor self)
+	{
+		var remaining = this.PowerGenerated;
+
+		if (this.DevMode.UnlimitedPower)
+			remaining = int.MaxValue;
+
+		for (var i = 0; i < this.Powers.Count; i++)
+		{
+			var (actor, power) = this.Powers.ElementAt(i);
+
+			if (power.IsTraitDisabled)
+			{
+				power.SetPowered(actor, false);
+
+				continue;
+			}
+
+			var consume = -power.Info.Amount;
+
+			if (consume <= 0)
+			{
+				power.SetPowered(actor, true);
+
+				continue;
+			}
+
+			remaining -= consume;
+
+			if (remaining >= 0)
+				power.SetPowered(actor, true);
+			else
+				power.SetPowered(actor, this.powerOn);
+		}
+
+		// Check, if there's enough power
+		if (remaining >= 0)
+		{
+			return PlayerPowerState.Ok;
+		}
+
+		if (self.World.WorldTick % this.info.PowerCycleInterval == 0)
+		{
+			this.powerOn = true;
+		}
+		else if (self.World.WorldTick % this.info.PowerCycleInterval == 0)
+		{
+			this.powerOn = false;
+		}
+
+		return PlayerPowerState.LowPower;
+	}
+}
