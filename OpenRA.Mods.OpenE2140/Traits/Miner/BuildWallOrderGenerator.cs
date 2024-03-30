3babef0a139bbab66a0d8a0af1a2150632ca2532
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-03-30 23:35:58 +0100

    Temporary panel for custom commands with build wall button

---
+++ OpenRA.Mods.OpenE2140/Traits/Miner/BuildWallOrderGenerator.cs
@@ -0,0 +1,81 @@
+﻿using OpenRA.Mods.Common.Orders;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Miner;
+
+public class BuildWallOrderGenerator : UnitOrderGenerator
+{
+	private TraitPair<WallBuilder>[] subjects;
+
+	public BuildWallOrderGenerator(IEnumerable<Actor> subjects)
+	{
+		this.subjects = GetWallBuilders(subjects);
+	}
+
+	public override IEnumerable<Order> Order(OpenRA.World world, CPos cell, int2 worldPixel, MouseInput mi)
+	{
+		if (mi.Button == Game.Settings.Game.MouseButtonPreference.Cancel)
+		{
+			world.CancelInputMode();
+			yield break;
+		}
+
+		if (mi.Button == Game.Settings.Game.MouseButtonPreference.Action)
+		{
+			var queued = mi.Modifiers.HasModifier(Modifiers.Shift);
+			if (!queued)
+			{
+				world.CancelInputMode();
+			}
+
+			yield return new Order(
+				WallBuilder.BuildWallOrderID,
+				null,
+				Target.FromCell(world, cell),
+				queued,
+				groupedActors: this.subjects.Select(p => p.Actor).ToArray());
+		}
+	}
+
+	public override void SelectionChanged(OpenRA.World world, IEnumerable<Actor> selected)
+	{
+		this.subjects = GetWallBuilders(selected);
+
+		if (!this.subjects.Any(s => s.Actor.Info.HasTraitInfo<AutoTargetInfo>()))
+			world.CancelInputMode();
+	}
+
+	private static TraitPair<WallBuilder>[] GetWallBuilders(IEnumerable<Actor> actors)
+	{
+		return actors
+			.Where(s => !s.IsDead)
+			.SelectMany(a => a.TraitsImplementing<WallBuilder>()
+				.Select(am => new TraitPair<WallBuilder>(a, am)))
+			.ToArray();
+	}
+
+	public override string? GetCursor(OpenRA.World world, CPos cell, int2 worldPixel, MouseInput mi)
+	{
+		var target = TargetForInput(world, cell, worldPixel, mi);
+
+		var subject = this.subjects.FirstOrDefault();
+		if (subject.Actor == null)
+		{
+			return null;
+		}
+
+		var isValid = subject.Trait.IsCellAcceptable(subject.Actor, world.Map.CellContaining(target.CenterPosition));
+		if (target.Actor != null)
+		{
+			isValid &= target.Actor == subject.Actor;
+		}
+
+		return isValid ? subject.Trait.Info.BuildCursor : subject.Trait.Info.BuildBlockedCursor;
+	}
+
+	public override bool InputOverridesSelection(OpenRA.World world, int2 xy, MouseInput mi)
+	{
+		return true;
+	}
+}
---
+++ OpenRA.Mods.OpenE2140/Widgets/Logic/CustomCommandsLogic.cs
@@ -0,0 +1,58 @@
+﻿using OpenRA.Mods.Common.Widgets;
+using OpenRA.Mods.OpenE2140.Traits.Miner;
+using OpenRA.Widgets;
+
+namespace OpenRA.Mods.OpenE2140.Widgets.Logic;
+
+/// <summary>
+/// Logic for custom commands used in OpenE2140.
+/// </summary>
+public class CustomCommandsLogic : ChromeLogic
+{
+	private int? selectionHash;
+	private Actor[] selectedActors = Array.Empty<Actor>();
+	private bool buildWallDisabled = true;
+
+	[ObjectCreator.UseCtor]
+	public CustomCommandsLogic(Widget widget, World world)
+	{
+		var buildWallButton = widget.GetOrNull<ButtonWidget>("BUILD_WALL");
+		if (buildWallButton != null)
+		{
+			WidgetUtils.BindButtonIcon(buildWallButton);
+
+			buildWallButton.IsDisabled = () => { UpdateStateIfNecessary(); return this.buildWallDisabled; };
+			buildWallButton.IsHighlighted = () => world.OrderGenerator is BuildWallOrderGenerator;
+
+			void Toggle(bool allowCancel)
+			{
+				if (buildWallButton.IsHighlighted())
+				{
+					if (allowCancel)
+						world.CancelInputMode();
+				}
+				else
+				{
+					world.OrderGenerator = new BuildWallOrderGenerator(this.selectedActors);
+				}
+			}
+
+			buildWallButton.OnClick = () => Toggle(true);
+			buildWallButton.OnKeyPress = _ => Toggle(false);
+		}
+
+		void UpdateStateIfNecessary()
+		{
+			if (this.selectionHash == world.Selection.Hash)
+				return;
+
+			this.selectedActors = world.Selection.Actors
+				.Where(a => a.Owner == world.LocalPlayer && a.IsInWorld && !a.IsDead)
+				.ToArray();
+
+			this.buildWallDisabled = !this.selectedActors.Any(a => a.Info.HasTraitInfo<WallBuilderInfo>() );
+
+			this.selectionHash = world.Selection.Hash;
+		}
+	}
+}
