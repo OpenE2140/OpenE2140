f26fdb5814a9a9212d52b2f7ea48d5d6cc006602
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-08-11 18:10:08 +0200

    Extension method for Animation

---
+++ OpenRA.Mods.OpenE2140/Extensions/AnimationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using OpenRA.Graphics;
+
+namespace OpenRA.Mods.OpenE2140.Extensions;
+
+public static class AnimationExtensions
+{
+	public static bool IsPlayingSequence(this Animation animation, string sequence)
+	{
+		return sequence.Equals(animation.CurrentSequence?.Name, StringComparison.Ordinal);
+	}
+}
