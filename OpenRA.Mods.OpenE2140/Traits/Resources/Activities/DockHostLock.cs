9a73b226e691d5c47e027069870d2bcf5b560eb3
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-09-18 21:24:21 +0200

    SharedDockHost

---
+++ OpenRA.Mods.OpenE2140/Traits/Resources/Activities/DockHostLock.cs
@@ -0,0 +1,60 @@
+﻿using OpenRA.Activities;
+using OpenRA.Mods.Common.Activities;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Resources.Activities;
+
+public class DockHostLock : Activity
+{
+	private readonly SharedDockHost sharedDockHost;
+	private readonly Activity dockActivity;
+	private readonly bool releaseOnFinish;
+
+	private bool hasDockStarted;
+	private bool wasCanceled;
+
+	public DockHostLock(SharedDockHost sharedDockHost, Activity dockActivity, bool releaseOnFinish = true)
+	{
+		this.sharedDockHost = sharedDockHost;
+		this.dockActivity = dockActivity;
+		this.releaseOnFinish = releaseOnFinish;
+		this.ChildHasPriority = false;
+	}
+
+	public override bool Tick(Actor self)
+	{
+		if (this.IsCanceling)
+		{
+			// Let child activities properly finish (undock, drag out, etc.)
+			if (!this.TickChild(self))
+				return false;
+
+			this.wasCanceled = true;
+
+			return true;
+		}
+
+		if (!this.TickChild(self))
+			return false;
+
+		if (!this.hasDockStarted)
+		{
+			if (!this.sharedDockHost.TryAcquireLock(self))
+			{
+				this.QueueChild(new Wait(5));
+				return false;
+			}
+
+			this.hasDockStarted = true;
+			this.QueueChild(this.dockActivity);
+			return false;
+		}
+
+		return true;
+	}
+
+	protected override void OnLastRun(Actor self)
+	{
+		if (this.hasDockStarted && (this.releaseOnFinish || this.wasCanceled))
+			this.sharedDockHost.ReleaseLock(self);
+	}
+}
---
+++ OpenRA.Mods.OpenE2140/Traits/Resources/SharedDockHost.cs
@@ -0,0 +1,90 @@
+﻿using OpenRA.Activities;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.OpenE2140.Traits.Resources.Activities;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Resources;
+
+[Desc("A version of dock that shares docking position with other SharedDockHosts.")]
+public class SharedDockHostInfo : DockHostInfo, IDockHostInfo, Requires<ISharedDockHostManagerInfo>
+{
+	[Desc("Docking type group.")]
+	public readonly BitSet<DockType> GroupType;
+
+	public override object Create(ActorInitializer init)
+	{
+		return new SharedDockHost(init.Self, this);
+	}
+}
+
+public class SharedDockHost : DockHost
+{
+	public new readonly SharedDockHostInfo Info;
+
+	private readonly ISharedDockHostManager manager;
+
+	private SharedDockHost[] otherDockHosts = Array.Empty<SharedDockHost>();
+	private Actor? currentClientActor;
+
+	public SharedDockHost(Actor self, SharedDockHostInfo info)
+		: base(self, info)
+	{
+		this.Info = info;
+		this.manager = self.Trait<ISharedDockHostManager>();
+	}
+
+	protected override void Created(Actor self)
+	{
+		this.otherDockHosts = self.TraitsImplementing<SharedDockHost>()
+			.Where(h => this.Info.GroupType.Overlaps(h.Info.GroupType) && h != this)
+			.ToArray();
+	}
+
+	public override void QueueDockActivity(Activity moveToDockActivity, Actor self, Actor clientActor, DockClientManager client)
+	{
+		moveToDockActivity.QueueChild(new DockHostLock(this,
+			new GenericDockSequence(
+				clientActor,
+				client,
+				self,
+				this,
+				this.Info.DockWait,
+				this.Info.IsDragRequired,
+				this.Info.DragOffset,
+				this.Info.DragLength)));
+	}
+
+	public override bool IsDockingPossible(Actor clientActor, IDockClient client, bool ignoreReservations = false)
+	{
+		if (!this.manager.IsDockingPossible(this, clientActor, client, ignoreReservations))
+			return false;
+
+		return base.IsDockingPossible(clientActor, client, ignoreReservations);
+	}
+
+	internal bool TryAcquireLock(Actor clientActor)
+	{
+		if ((this.currentClientActor == null)
+			&& this.otherDockHosts.All(h => h.currentClientActor == null))
+		{
+			this.currentClientActor = clientActor;
+
+			Array.ForEach(this.otherDockHosts, h => h.currentClientActor = clientActor);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	internal void ReleaseLock(Actor clientActor)
+	{
+		if (this.currentClientActor == clientActor)
+		{
+			this.currentClientActor = null;
+			Array.ForEach(this.otherDockHosts, h => h.currentClientActor = null);
+		}
+	}
+}
