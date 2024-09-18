9d03f42107a05c741d7e9375b7ad625df16020b5
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-09-18 21:24:21 +0200

    SharedDockHost

---
+++ OpenRA.Mods.OpenE2140/Traits/Resources/SharedDockHostManager.cs
@@ -0,0 +1,25 @@
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Resources;
+
+public interface ISharedDockHostManagerInfo : ITraitInfoInterface { }
+
+public interface ISharedDockHostManager
+{
+	bool IsDockingPossible(SharedDockHost sharedDockHost, Actor clientActor, IDockClient client, bool ignoreReservations = false);
+}
+
+public class SharedDockHostManager<InfoType> : PausableConditionalTrait<InfoType>, ISharedDockHostManager
+	where InfoType : PausableConditionalTraitInfo
+{
+	public SharedDockHostManager(InfoType info)
+		: base(info)
+	{
+	}
+
+	public virtual bool IsDockingPossible(SharedDockHost sharedDockHost, Actor clientActor, IDockClient client, bool ignoreReservations = false)
+	{
+		return true;
+	}
+}
