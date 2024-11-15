b0f1011b32ac0054453d2d439040d8a9af0eb200
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-11-15 17:36:40 +0100

    Move locking to ISharedDockHostManager

--- OpenRA.Mods.OpenE2140/Traits/Resources/SharedDockHostManager.cs
+++ OpenRA.Mods.OpenE2140/Traits/Resources/SharedDockHostManager.cs
@@ -8,11 +8,17 @@ public interface ISharedDockHostManagerInfo : ITraitInfoInterface { }
 public interface ISharedDockHostManager
 {
 	bool IsDockingPossible(SharedDockHost sharedDockHost, Actor clientActor, IDockClient client, bool ignoreReservations = false);
+
+	bool TryAcquireLock(Actor clientActor);
+
+	void TryReleaseLock(Actor clientActor);
 }
 
 public class SharedDockHostManager<InfoType> : PausableConditionalTrait<InfoType>, ISharedDockHostManager
 	where InfoType : PausableConditionalTraitInfo
 {
+	public Actor? CurrentClientActor { get; private set; }
+
 	public SharedDockHostManager(InfoType info)
 		: base(info)
 	{
@@ -21,5 +27,34 @@ public class SharedDockHostManager<InfoType> : PausableConditionalTrait<InfoType
 	public virtual bool IsDockingPossible(SharedDockHost sharedDockHost, Actor clientActor, IDockClient client, bool ignoreReservations = false)
 	{
 		return true;
+	}
+
+	public bool TryAcquireLock(Actor clientActor)
+	{
+		if (this.CurrentClientActor == clientActor)
+			return true;
+
+		if (!this.TryAcquireLockInner(clientActor))
+			return false;
+
+		if (this.CurrentClientActor == null)
+		{
+			this.CurrentClientActor = clientActor;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	protected virtual bool TryAcquireLockInner(Actor clientActor)
+	{
+		return true;
+	}
+
+	public void TryReleaseLock(Actor clientActor)
+	{
+		if (this.CurrentClientActor == clientActor)
+			this.CurrentClientActor = null;
 	}
 }
