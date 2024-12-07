7441831fa6a3000cfe1a4545a74a80f428aab938
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-12-07 19:53:36 +0100

    Split CrateTransporter into two inherited classes: one for Mobile, the other for Aircraft

---
+++ OpenRA.Mods.OpenE2140/Traits/Resources/AircraftCrateTransporter.cs
@@ -0,0 +1,28 @@
+﻿using OpenRA.Activities;
+using OpenRA.Mods.OpenE2140.Traits.Resources.Activities;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Resources;
+
+public class AircraftCrateTransporterInfo : CrateTransporterInfo
+{
+	public override object Create(ActorInitializer init)
+	{
+		return new AircraftCrateTransporter(init, this);
+	}
+}
+
+public class AircraftCrateTransporter : CrateTransporter
+{
+	public new AircraftCrateTransporterInfo Info;
+
+	public AircraftCrateTransporter(ActorInitializer init, AircraftCrateTransporterInfo info)
+		: base(init, info)
+	{
+		this.Info = info;
+	}
+
+	protected override Activity GetCrateLoadActivity(Actor self, Order order)
+	{
+		return new AircraftCrateLoad(self, order.Target);
+	}
+}
