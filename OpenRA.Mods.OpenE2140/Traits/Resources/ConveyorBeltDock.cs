ea1945a22d2a2c5f2646812e9ae3b0875dc32ce8
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-12-13 22:33:44 +0100

    Fix for blocked dock position

--- OpenRA.Mods.OpenE2140/Traits/Resources/ConveyorBeltDock.cs
+++ OpenRA.Mods.OpenE2140/Traits/Resources/ConveyorBeltDock.cs
@@ -1,4 +1,5 @@
 ﻿using OpenRA.Activities;
+using OpenRA.Mods.Common.Traits;
 using OpenRA.Mods.OpenE2140.Traits.Resources.Activities;
 
 namespace OpenRA.Mods.OpenE2140.Traits.Resources;
@@ -63,6 +64,19 @@ public class ConveyorBeltDock : SharedDockHost, IConveyorBeltDockHost
 	public ConveyorBeltDock(Actor self, ConveyorBeltDockInfo info)
 		: base(self, info)
 	{
+	}
+
+	public override bool IsDockingPossible(Actor clientActor, IDockClient client, bool ignoreReservations = false)
+	{
+		if (!base.IsDockingPossible(clientActor, client, ignoreReservations))
+			return false;
+
+		// TODO: this should be temporary until this issue solved in the engine:
+		// - if an immovable actor is located on the dock cell, the dock client does not try to find another dock (and is blocked)
+		// - this workaround makes the dock client completely ignore this dock host, if there's an immovable actor at the dock cell
+		return !clientActor.World.ActorMap
+			.GetActorsAt(clientActor.World.Map.CellContaining(this.DockPosition))
+			.Any(a => !a.Info.HasTraitInfo<CrateTransporterInfo>() && !a.Info.HasTraitInfo<MobileInfo>());
 	}
 
 	Activity IConveyorBeltDockHost.GetInnerDockActivity(Actor self, Actor clientActor, Action continuationCallback, ConveyorBeltInnerDockContext context)
