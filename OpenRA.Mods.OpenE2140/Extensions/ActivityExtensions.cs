ae92a939a28d20a83567867292fe62568dc7b1ae
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-12-17 22:46:06 +0100

    Extension methods for queuing activities

---
+++ OpenRA.Mods.OpenE2140/Extensions/ActivityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using OpenRA.Activities;
+
+namespace OpenRA.Mods.OpenE2140.Extensions;
+
+public static class ActivityExtensions
+{
+	public static Activity WithChild(this Activity parent, Activity child)
+	{
+		parent.QueueChild(child);
+		return parent;
+	}
+
+	public static Activity WithNext(this Activity current, Activity next)
+	{
+		current.Queue(next);
+		return current;
+	}
+}
