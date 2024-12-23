78c27163b4f832f44fe770f9d6485a62add79735
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-12-23 22:18:07 +0100

    Mine: allow partial overlap with ore field cells

---
+++ OpenRA.Mods.OpenE2140/Traits/Resources/ResourceMineCustomBuilding.cs
@@ -0,0 +1,26 @@
+﻿using OpenRA.Mods.Common.Orders;
+using OpenRA.Mods.OpenE2140.Traits.Mcu;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Resources;
+
+public class ResourceMineCustomBuildingInfo : CustomBuildingInfo
+{
+	public override Dictionary<CPos, PlaceBuildingCellType> GetBuildingPlacementFootprint(OpenRA.World world, CPos cell, Actor toIgnore)
+	{
+		var footprint = base.GetBuildingPlacementFootprint(world, cell, toIgnore);
+
+		var footprintCellTypes = new Dictionary<string, int>();
+		foreach ((var c, var type) in footprint)
+		{
+			var terrainType = world.Map.GetTerrainInfo(c).Type;
+			footprintCellTypes.TryGetValue(terrainType, out var count);
+			footprintCellTypes[terrainType] = ++count;
+		}
+
+		// Next check, if cells of the footprint contain valid terrain types
+		if (this.AllowedTerrainTypesCondition?.Evaluate(footprintCellTypes) == false)
+			return footprint.Keys.ToDictionary(c => c, _ => PlaceBuildingCellType.Invalid);
+
+		return footprint;
+	}
+}
