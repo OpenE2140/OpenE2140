869cc2cee0f366bcc7bf146b04b1ccb310615a18
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-12-27 18:06:17 +0100

    ResourceRefinery delay crate processing

--- OpenRA.Mods.OpenE2140/Traits/Resources/ResourceRefinery.cs
+++ OpenRA.Mods.OpenE2140/Traits/Resources/ResourceRefinery.cs
@@ -12,7 +12,10 @@
 #endregion
 
 using JetBrains.Annotations;
+using OpenRA.Activities;
+using OpenRA.Mods.Common.Activities;
 using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.OpenE2140.Extensions;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.OpenE2140.Traits.Resources;
@@ -21,6 +24,16 @@ namespace OpenRA.Mods.OpenE2140.Traits.Resources;
 [Desc("This actor can accept resource crates and process them.")]
 public class ResourceRefineryInfo : ConveyorBeltInfo
 {
+	[Desc("Additional time required for crate processing after the crate disappears inside the Refinery.")]
+	public readonly int DelayCrateProcessing = 50;
+
+	[Desc("Maximum number of crates Refinery can buffer for processing.")]
+	public readonly int BufferCapacity = 1;
+
+	[GrantedConditionReference]
+	[Desc("Condition to grant while a resource crate is being processed.")]
+	public readonly string ProcessCrateCondition = "ProcessingCrate";
+
 	public override object Create(ActorInitializer init)
 	{
 		return new ResourceRefinery(this);
@@ -29,11 +42,20 @@ public class ResourceRefineryInfo : ConveyorBeltInfo
 
 public class ResourceRefinery : ConveyorBelt, INotifyAddedToWorld, INotifyOwnerChanged
 {
-	private PlayerResources? playerResources;
+	private readonly ResourceRefineryInfo info;
+	private readonly List<ResourceCrate> processingBuffer;
 
-	public ResourceRefinery(ConveyorBeltInfo info)
+	private PlayerResources? playerResources;
+	private int crateProcessingToken = Actor.InvalidConditionToken;
+
+	private int BufferCapacity => this.info.BufferCapacity;
+
+	public ResourceRefinery(ResourceRefineryInfo info)
 		: base(info)
 	{
+		this.info = info;
+
+		this.processingBuffer = new(this.BufferCapacity);
 	}
 
 	void INotifyAddedToWorld.AddedToWorld(Actor self)
@@ -46,11 +68,70 @@ public class ResourceRefinery : ConveyorBelt, INotifyAddedToWorld, INotifyOwnerC
 		this.playerResources = newOwner.PlayerActor.TraitOrDefault<PlayerResources>();
 	}
 
+	protected override bool ActivateInner(Actor self, ResourceCrate crate)
+	{
+		if (this.processingBuffer.Count >= this.BufferCapacity + 1)
+			return false;
+
+		this.processingBuffer.Add(crate);
+
+		self.QueueActivity(new ProcessCrate(crate, this));
+
+		return true;
+	}
+
+	protected override bool TryProgress(Actor self, ResourceCrate crate)
+	{
+		return this.processingBuffer.Count <= this.BufferCapacity;
+	}
+
 	protected override void Complete(Actor self, ResourceCrate crate)
 	{
-		this.playerResources?.GiveCash(crate.Resources);
-		crate.Actor.Dispose();
-
 		this.OnCrateProcessed();
+		crate.Actor.Dispose();
+	}
+
+	protected override bool TryAcquireLockInner(Actor clientActor)
+	{
+		return this.processingBuffer.Count <= this.BufferCapacity + 1 && !this.HasCrate;
+	}
+
+	private class ProcessCrate : Activity
+	{
+		private readonly ResourceCrate resourceCrate;
+		private readonly ResourceRefinery resourceRefinery;
+
+		public ProcessCrate(ResourceCrate resourceCrate, ResourceRefinery resourceRefinery)
+		{
+			this.resourceCrate = resourceCrate;
+			this.resourceRefinery = resourceRefinery;
+		}
+
+		protected override void OnFirstRun(Actor self)
+		{
+			this.resourceRefinery.playerResources?.GiveCash(this.resourceCrate.Resources);
+
+			self.TryGrantingCondition(ref this.resourceRefinery.crateProcessingToken, this.resourceRefinery.info.ProcessCrateCondition);
+		}
+
+		public override bool Tick(Actor self)
+		{
+			if (this.resourceCrate.Actor.Disposed)
+			{
+				this.QueueChild(new Wait(this.resourceRefinery.info.DelayCrateProcessing));
+
+				return true;
+			}
+
+			return false;
+		}
+
+		protected override void OnLastRun(Actor self)
+		{
+			this.resourceRefinery.processingBuffer.Remove(this.resourceCrate);
+
+			if (this.NextActivity is not ProcessCrate)
+				self.TryRevokingCondition(ref this.resourceRefinery.crateProcessingToken);
+		}
 	}
 }
