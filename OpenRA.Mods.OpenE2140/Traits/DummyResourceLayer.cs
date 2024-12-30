9df05bd2029e282bc3c8fd1a4b3e9e125427db27
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-12-30 21:41:44 +0100

    Replace standard ResourceLayer with dummy implementation
    Fixes crash when saving map in the map editor. Standard ResourceLayer depends on proper configuration (including tilesets)

---
+++ OpenRA.Mods.OpenE2140/Traits/DummyResourceLayer.cs
@@ -0,0 +1,72 @@
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits;
+
+public class DummyResourceLayerInfo : TraitInfo, IResourceLayerInfo
+{
+	public override object Create(ActorInitializer init)
+	{
+		return new DummyResourceLayer(this);
+	}
+
+	bool IResourceLayerInfo.TryGetResourceIndex(string resourceType, out byte index)
+	{
+		index = 0;
+		return false;
+	}
+
+	bool IResourceLayerInfo.TryGetTerrainType(string resourceType, out string terrainType)
+	{
+		terrainType = string.Empty;
+		return false;
+	}
+}
+
+public class DummyResourceLayer : IResourceLayer
+{
+	public DummyResourceLayer(DummyResourceLayerInfo info)
+	{
+		this.Info = info;
+	}
+
+	bool IResourceLayer.IsEmpty => true;
+
+	public IResourceLayerInfo Info { get; }
+
+	event Action<CPos, string> IResourceLayer.CellChanged { add { } remove { } }
+
+	int IResourceLayer.AddResource(string resourceType, CPos cell, int amount)
+	{
+		return 0;
+	}
+
+	bool IResourceLayer.CanAddResource(string resourceType, CPos cell, int amount)
+	{
+		return false;
+	}
+
+	void IResourceLayer.ClearResources(CPos cell)
+	{
+	}
+
+	int IResourceLayer.GetMaxDensity(string resourceType)
+	{
+		return 0;
+	}
+
+	ResourceLayerContents IResourceLayer.GetResource(CPos cell)
+	{
+		return new ResourceLayerContents();
+	}
+
+	bool IResourceLayer.IsVisible(CPos cell)
+	{
+		return false;
+	}
+
+	int IResourceLayer.RemoveResource(string resourceType, CPos cell, int amount)
+	{
+		return 0;
+	}
+}
