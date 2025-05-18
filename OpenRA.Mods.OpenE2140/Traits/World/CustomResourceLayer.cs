8dd868d3fafe687d377d41bf21b37e87a1184101
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2025-05-18 20:00:01 +0200

    Use custom version of ResourceLayer that allows placing resources underneath buildings with AllowPlacementOnResources: true
    Fixes #607

---
+++ OpenRA.Mods.OpenE2140/Traits/World/CustomResourceLayer.cs
@@ -0,0 +1,37 @@
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.OpenE2140.Extensions;
+
+namespace OpenRA.Mods.OpenE2140.Traits.World;
+
+public class CustomResourceLayerInfo : ResourceLayerInfo
+{
+	public override object Create(ActorInitializer init)
+	{
+		return new CustomResourceLayer(init.Self, this);
+	}
+}
+
+public class CustomResourceLayer : ResourceLayer
+{
+	private readonly ResourceLayerInfo info;
+
+	public CustomResourceLayer(Actor self, ResourceLayerInfo info)
+		: base(self, info)
+	{
+		this.info = info;
+	}
+
+	protected override bool AllowResourceAt(string resourceType, CPos cell)
+	{
+		if (!this.Map.Contains(cell) || this.Map.Ramp[cell] != 0)
+			return false;
+
+		if (resourceType == null || !this.info.ResourceTypes.TryGetValue(resourceType, out var resourceInfo))
+			return false;
+
+		if (!resourceInfo.AllowedTerrainTypes.Contains(this.Map.GetTerrainInfo(cell).Type))
+			return false;
+
+		return this.BuildingInfluence.GetBuildingsAt(cell).All(a => !a.TryGetTrait<Building>(out var building) || building.Info.AllowPlacementOnResources);
+	}
+}
