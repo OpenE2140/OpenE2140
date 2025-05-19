af4ecf5c08984b2752ff68af8a0f27e09fe70f14
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2025-05-19 22:04:04 +0200

    Add Deconstruct() method for TraitPair<T>

---
+++ OpenRA.Mods.OpenE2140/Extensions/TraitPairExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenRA.Mods.OpenE2140.Extensions;
+
+public static class TraitPairExtensions
+{
+	public static void Deconstruct<T>(this TraitPair<T> traitPair, out Actor actor, out T trait)
+	{
+		actor = traitPair.Actor;
+		trait = traitPair.Trait;
+	}
+}
