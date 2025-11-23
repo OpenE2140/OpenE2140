e1c0e210f5fe8374bbe3db0f5d761ad52cbb8ae1
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2025-11-23 16:58:02 +0100

    Fix temporarily disabled repulsion for Heavy Lifter
    Use custom Aircraft trait for Heavy Lifter, which temporarily disables
    repulsion when close to dock. Currently uses
    WaitDistanceFromResupplyBase from AircraftInfo.

---
+++ OpenRA.Mods.OpenE2140/Traits/Resources/CrateTransporterAircraft.cs
@@ -0,0 +1,46 @@
+﻿using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Resources;
+
+public class CrateTransporterAircraftInfo : AircraftInfo
+{
+	public override object Create(ActorInitializer init)
+	{
+		return new CrateTransporterAircraft(init, this);
+	}
+}
+
+public class CrateTransporterAircraft : Aircraft
+{
+	private readonly Actor self;
+
+	private DockClientManager? dockClientManager;
+
+	public CrateTransporterAircraft(ActorInitializer init, AircraftInfo info)
+		: base(init, info)
+	{
+		this.self = init.Self;
+	}
+
+	protected override void Created(Actor self)
+	{
+		base.Created(self);
+
+		this.dockClientManager = self.Trait<DockClientManager>();
+	}
+
+	public override WVec GetRepulsionForce()
+	{
+		// The logic in Aircraft.GetRepulsionForce(), that temporarily disables the repulsion,
+		// currently works only with Reservable trait and not the generic dock infrastructure.
+		var dockHostActor = this.dockClientManager?.ReservedHostActor;
+		if (dockHostActor != null)
+		{
+			var distanceFromDock = (dockHostActor.CenterPosition - this.self.CenterPosition).HorizontalLength;
+			if (distanceFromDock < this.Info.WaitDistanceFromResupplyBase.Length)
+				return WVec.Zero;
+		}
+
+		return base.GetRepulsionForce();
+	}
+}
