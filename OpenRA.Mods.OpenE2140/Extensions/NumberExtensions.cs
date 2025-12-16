0e3cf9a43a01e1e04fe74bff037b9d587e5f8f58
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2025-12-16 20:41:56 +0100

    IsBetween<INumber>() extension method

---
+++ OpenRA.Mods.OpenE2140/Extensions/NumberExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace OpenRA.Mods.OpenE2140.Extensions;
+
+public static class NumberExtensions
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsBetween<T>(this T number, T lowerBound, T upperBound)
+		where T : INumber<T>
+	{
+		return lowerBound <= number && number < upperBound;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T PowerOf2<T>(this T number)
+		where T : INumber<T>
+	{
+		return number * number;
+	}
+}
