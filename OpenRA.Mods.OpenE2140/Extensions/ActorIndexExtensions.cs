a3f70242bb9277aed10ae48ce47262bd940f034f
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2025-12-17 18:49:15 +0100

    OwnerAndTrait actor index

---
+++ OpenRA.Mods.OpenE2140/Extensions/ActorIndexExtensions.cs
@@ -0,0 +1,11 @@
+﻿using OpenRA.Mods.Common;
+
+namespace OpenRA.Mods.OpenE2140.Extensions;
+
+public static class ActorIndexExtensions
+{
+	public static IEnumerable<Actor> Alive(this ActorIndex index)
+	{
+		return index.Actors.Where(a => !a.IsDead);
+	}
+}
---
+++ OpenRA.Mods.OpenE2140/Utils/OwnerAndTraitIndex.cs
@@ -0,0 +1,35 @@
+﻿using OpenRA.Mods.Common;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Utils;
+
+public class OwnerAndTraitIndex<TTraitInfo> : ActorIndex
+	where TTraitInfo : ITraitInfoInterface
+{
+	private readonly HashSet<string> names;
+	private readonly Player owner;
+
+	public OwnerAndTraitIndex(World world, IReadOnlyCollection<string> names, Player owner)
+		: base(world, ActorsToIndex(world, names.ToHashSet(), owner))
+	{
+		this.names = names.ToHashSet();
+		this.owner = owner;
+	}
+
+	private static IEnumerable<Actor> ActorsToIndex(World world, HashSet<string> names, Player owner)
+	{
+		return world.Actors.Where(a => ShouldIndexActor(a, names, owner));
+	}
+
+	protected override bool ShouldIndexActor(Actor actor)
+	{
+		return ShouldIndexActor(actor, this.names, this.owner);
+	}
+
+	private static bool ShouldIndexActor(Actor actor, HashSet<string> names, Player owner)
+	{
+		return actor.Owner == owner
+			&& (names.Count == 0 || names.Contains(actor.Info.Name))
+			&& actor.Info.HasTraitInfo<TTraitInfo>();
+	}
+}
