a30492b6ed45e9ef2e1e6f120cd39d8fd05b3144
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2026-01-25 13:18:43 +0100

    Fix rendering rate in SpriteAnimationWidget

--- OpenRA.Mods.OpenE2140/Widgets/SpriteAnimationWidget.cs
+++ OpenRA.Mods.OpenE2140/Widgets/SpriteAnimationWidget.cs
@@ -1,4 +1,5 @@
-﻿using OpenRA.Graphics;
+﻿using System.Diagnostics;
+using OpenRA.Graphics;
 using OpenRA.Mods.Common.Widgets;
 using OpenRA.Widgets;
 
@@ -6,11 +7,13 @@ namespace OpenRA.Mods.OpenE2140.Widgets;
 
 public class SpriteAnimationWidget : Widget
 {
-	private int currentTick;
+	private readonly Stopwatch playTime = new();
+
 	private int currentFrame;
+	private int framerate;
+	private float invLength;
 	private Sprite[] spritesheet = [];
 
-	public int TickLength = 1;
 
 	public SpriteAnimationWidget() { }
 
@@ -19,36 +22,39 @@ public class SpriteAnimationWidget : Widget
 	{
 	}
 
-	public void SetSpriteSheet(Sprite[] spritesheet)
+	public void SetSpriteSheet(Sprite[] spritesheet, int framerate = 35)
 	{
 		this.spritesheet = spritesheet;
 
-		this.currentFrame = -1;
-		this.currentTick = this.TickLength;
+		this.currentFrame = 0;
+		this.framerate = Math.Clamp(framerate, 1, 200);
+		this.invLength = this.framerate * 1f / this.spritesheet.Length;
+		this.playTime.Restart();
 	}
 
 	public override SpriteAnimationWidget Clone() { return new SpriteAnimationWidget(this); }
 
-	public override void Tick()
-	{
-		if (--this.currentTick > 0)
-			return;
-
-		this.currentFrame = (this.currentFrame + 1) % this.spritesheet.Length;
-		this.currentTick = this.TickLength;
-	}
-
 	public override void Draw()
 	{
-		if (this.spritesheet.Length == 0 || this.currentFrame >= this.spritesheet.Length || this.currentFrame < 0)
+		if (this.spritesheet.Length == 0)
 			return;
 
-		var sprite = this.spritesheet[this.currentFrame];
-		if (sprite != null)
+		this.currentFrame = (int)float2.Lerp(0, this.spritesheet.Length, (float)this.playTime.Elapsed.TotalSeconds * this.invLength);
+		if (this.currentFrame >= this.spritesheet.Length)
 		{
-			var x = (this.RenderBounds.Width - sprite.Size.X) / 2 + this.RenderBounds.X;
-			var y = (this.RenderBounds.Height - sprite.Size.Y) / 2 + this.RenderBounds.Y;
-			WidgetUtils.DrawSprite(sprite, new float2(x, y));
+			this.currentFrame = 0;
+			this.playTime.Restart();
+		}
+
+		if (this.currentFrame >= 0 && this.currentFrame < this.spritesheet.Length)
+		{
+			var sprite = this.spritesheet[this.currentFrame];
+			if (sprite != null)
+			{
+				var x = (this.RenderBounds.Width - sprite.Size.X) / 2 + this.RenderBounds.X;
+				var y = (this.RenderBounds.Height - sprite.Size.Y) / 2 + this.RenderBounds.Y;
+				WidgetUtils.DrawSprite(sprite, new float2(x, y));
+			}
 		}
 	}
 }
